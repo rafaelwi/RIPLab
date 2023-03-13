9b6e43d760059f4761eff6a5d0d57d093d7e4f76
rafaelwi <rafwiskailnicki@gmail.com>
2023-03-12 23:40:45 -0400

    First attempt at connecting backend to frontend

--- api/api.py
+++ api/api.py
@@ -1,8 +1,14 @@
 import time
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+cors = CORS(app, resources={'/*':{'origins': 'http://localhost:3000'}}) 
+
+if __name__ == "__main__":
+    app.run()
